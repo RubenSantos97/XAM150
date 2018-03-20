69d8a14a7d33bda8c1539147fd3306184565aab4
Ruben-HP\Ruben Santos <rubenf_santos@hotmail.com>
2018-03-20 18:01:10 +0000

    no message

---
+++ Exercicio 1/NetStatus.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("NetStatus.Droid.MainApplication, NetStatus.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5fc5ec1789a2a63e01e602bb102220a91.MainApplication.class, md5fc5ec1789a2a63e01e602bb102220a91.MainApplication.__md_methods);
+		
+	}
+}
